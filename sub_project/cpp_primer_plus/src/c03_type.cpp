f8c4c2433b01b3781f84cbab412b683dfbdb7026
Yuanjun Zhong <937676516@qq.com>
2024-08-01 20:51:10 +0800

    [cpp_primer]: update c03 处理数据

--- sub_project/cpp_primer_plus/src/c03_type.cpp
+++ sub_project/cpp_primer_plus/src/c03_type.cpp
@@ -2,9 +2,27 @@
 
 int main() {
   using namespace std;
+
+  char a1 = 'a';
+  char a2 = 97;  // 与上者等效
+  cout << "a1==a2? " << (a1 == a2) << endl;
+
   int num = 12;
   cout << "12 的十六进制是 ";
-  cout << hex;
+  cout << hex;  // hex 可以让数字输出以 16进制 显示
   cout << num << endl;
+
+  cout << "输出 unicode: \u00ff" << endl;
+
+  const double PI = 3.14;  // 常量， 可用来替代 #define
+  cout << "PI = " << PI << endl;
+
+  unsigned int x1{312};  // 使用 {} 初始化时，可以检查大数赋值给小的类型，
+                         // 比如说， x1 为char时，会编译错误
+  long double x2 = 12;
+  auto x0 =
+      x1 + x2;  // auto 可以自动推断类型, 不同类型的做运算， 会转成相同类型
+  cout << "x0=" << x0 << endl;
+
   return 0;
 }
