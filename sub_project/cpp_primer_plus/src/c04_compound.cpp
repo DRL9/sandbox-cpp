0c59741966d977684ee2c43914482b54f832e41a
Yuanjun Zhong <937676516@qq.com>
2024-08-03 14:33:50 +0800

    [cpp_primer]: update c04 复合类型

--- sub_project/cpp_primer_plus/src/c04_compound.cpp
+++ sub_project/cpp_primer_plus/src/c04_compound.cpp
@@ -1,5 +1,8 @@
+#include <array>
 #include <cstring>
 #include <iostream>
+#include <string>
+#include <vector>
 
 int main() {
   using namespace std;
@@ -15,5 +18,80 @@ int main() {
   cin.getline(name, 10);  // 最多读 10 个字节
   cout << name << endl;
 
+  char name2[32];  // 数组大小一定要够
+  //   复制需要用到 copy
+  strcpy(name2, name);
+  strcat(name2, ", good morning");  // 拼接
+  cout << name2 << endl;
+
+  // 字符串
+  string str3 = "hello中";
+  cout << "字节数: " << str3.size() << endl;
+
+  // 原始字符串, 会不转义字符
+  string str4 = R"(Lily say: "how are you"\n)";
+  cout << str4 << endl;
+
+  struct book {
+    string name;
+    int price;
+  };
+  auto book1 = book{"How to success.", 100};
+  cout << "name: " << book1.name << ", price: " << book1.price << endl;
+
+  // 其成员占用同一内存空间， 内存大小为最大的数据类型， 主要用来节省空间
+  union unit {
+    int int_val;
+    long long_val;
+  };
+
+  auto unit1 = unit{
+      1,
+  };
+  unit1.long_val = 2;  // int_val 的值会消失
+  cout << "unit size: " << sizeof(unit) << ", val:" << unit1.int_val << "|"
+       << unit1.long_val << endl;
+
+  // 枚举
+  // 默认从0 算起
+  enum Color { Red, Green, Blue = 100, Yellow };
+  cout << "red: " << Color::Red << ",Yellow: " << Color::Yellow << endl;
+
+  // 指针
+  book* ptr1 = &book1;
+  cout << "book1'address: " << ptr1 << endl;
+  cout << "book'price:" << ptr1->price << ", name:" << (*ptr1).name << endl;
+
+  // 在堆上分配内存
+  book* book2 = new book{"Cookbook", 1222};
+  cout << "指针大小： " << sizeof(book2) << ", 数据大小：" << sizeof(*book2)
+       << endl;
+  // new 需要对应 delete 释放内存
+  delete book2;
+
+  //  创建动态数组
+  long* ptr2 = new long[12];
+  ptr2[0] = 1;
+  ptr2[1] = 12;
+  //   指针指向数字的第一个元素
+  cout << "0: " << ptr2[0] << ", 0: " << *ptr2 << endl;
+  ptr2++;
+  cout << "1: " << ptr2[0] << endl;
+  //   需要减回去才能释放
+  ptr2--;
+  delete[] ptr2;
+
+  // 其它数组
+  //   可以动态指定大小
+  int size1 = 10;
+  //   储存在堆中
+  vector<int> v1(size1);
+  v1[0] = 12;
+  cout << "vector[1]: " << v1[0] << endl;
+
+  // 储存在栈上
+  array<int, 10> a2 = {1, 2};
+  cout << "array[1]: " << a2[0] << endl;
+  //   a2.at(100); // 数组越界会中断程序
   return 0;
 }
